5f0ae9354f5be522414ef2e3d933f6ad82cf0e39
Sachin Ranghar <sachinranghar14@gmail.com>
2025-04-06 15:51:00 +0530

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,13 +1,14 @@
 function exportCSV() {
-  let csv = "Text,Amount,Date,Category\n";
-  transactions.forEach(t => {
-    csv += `${t.text},${t.amount},${t.date},${t.category}\n`;
-  });
-
-  const blob = new Blob([csv], { type: 'text/csv' });
-  const url = window.URL.createObjectURL(blob);
-  const a = document.createElement('a');
-  a.href = url;
-  a.download = 'budget.csv';
-  a.click();
-}
+    let csv = "Text,Amount,Date,Category\n";
+    transactions.forEach(t => {
+      csv += `${t.text},${t.amount},${t.date},${t.category}\n`;
+    });
+  
+    const blob = new Blob([csv], { type: 'text/csv' });
+    const url = window.URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = 'budget.csv';
+    a.click();
+  }
+  
